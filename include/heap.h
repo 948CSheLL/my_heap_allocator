8eef8a79d55d009e6c65c97136b1e6ab8eb8bfdf
948CSheLL <2418942810@qq.com>
2022-07-06 03:51:51 -0700

    modify include

--- include/heap.h
+++ include/heap.h
@@ -18,6 +18,7 @@
 
 typedef unsigned int uint;
 
+/* 定义的块的结构 */
 typedef struct node_t {
     uint hole;              // 块是否空闲
     uint size;              // 块的大小
@@ -25,31 +26,43 @@ typedef struct node_t {
     struct node_t* prev;
 } node_t;
 
+/* 定义的页脚结构 */
 typedef struct { 
-    node_t *header;
+    node_t *header;         // 指向页眉
 } footer_t;
 
+/* 定义的空闲链表结构 */
 typedef struct {
-    node_t* head;
+    node_t* head;           // 指向第一个空闲块
 } bin_t;
 
+/* 定义的堆结构 */
 typedef struct {
     long start;
     long end;
     bin_t *bins[BIN_COUNT];
 } heap_t;
 
+/* 页眉和页脚的大小之和 */
 static uint overhead = sizeof(footer_t) + sizeof(node_t);
 
+/* 初始化堆 */
 void init_heap(heap_t *heap, long start);
 
+/* 从堆中分配size 大小的空间 */
 void *heap_alloc(heap_t *heap, size_t size);
+/* 向堆中添加之前分配的空间 */
 void heap_free(heap_t *heap, void *p);
+/* 扩展整个堆，此函数没有实现 */
 uint expand(heap_t *heap, size_t sz);
+/* 缩小整个堆，此函数没有实现 */
 void contract(heap_t *heap, size_t sz);
 
+/* 根据大小选择对应的空闲链表下标 */
 uint get_bin_index(size_t sz);
+/* 给一个块创建一个页脚 */
 void create_foot(node_t *head);
+/* 获得一个指向页脚的指针 */
 footer_t *get_foot(node_t *head);
 
 node_t *get_wilderness(heap_t *heap);
--- include/llist.h
+++ include/llist.h
@@ -4,11 +4,15 @@
 #include "heap.h"
 #include <stdint.h>
 
+/* 将一个块添加回空闲链表 */
 void add_node(bin_t *bin, node_t *node);
 
+/* 将一个块从空闲链表中移除 */
 void remove_node(bin_t *bin, node_t *node);
 
+/* 找到大小最合适的块 */
 node_t *get_best_fit(bin_t *list, size_t size);
+/* 最后三个函数没用过 */
 node_t *get_last_node(bin_t *list);
 
 node_t *next(node_t *current);
